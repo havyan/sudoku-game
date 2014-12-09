58b1c8991d157537a8341813794be5bad73f97a2
havyan <havyan@gmail.com>
2014-12-09 22:49:46 +0800

    Pending

--- public/js/components/game_panel.js
+++ public/js/components/game_panel.js
@@ -1,11 +1,18 @@
 (function() {
 	can.Control('Components.GamePanel', {}, {
-		init: function(element, options) {
+		init : function(element, options) {
 			element.html(can.view('mst/game_panel.mst', options.model));
 		},
-	
-		'.game-state-hide-button click': function() {
-			this.element.find('.game-state-area').css('right', '-webkit-calc(-13%-20px)');
+
+		'.game-state-hide-button click' : function() {
+			var gameStateArea = this.element.find('.game-state-area');
+			if (gameStateArea.hasClass('out')) {
+				gameStateArea.removeClass('out').css('right', '0px');
+				gameStateArea.find('.game-state-hide-button').html('>>');
+			} else {
+				gameStateArea.addClass('out').css('right', (-gameStateArea[0].offsetWidth) + 'px');
+				gameStateArea.find('.game-state-hide-button').html('<<');
+			}
 		}
 	});
 })();
