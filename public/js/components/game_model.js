d584c936090972dc7906adb02c2e051a0898dea8
havyan <havyan@gmail.com>
2014-12-11 00:28:50 +0800

    Pending

--- public/js/components/game_model.js
+++ public/js/components/game_model.js
@@ -1,65 +1,76 @@
 (function() {
 	can.Model('Models.GameModel', {}, {
 		init : function() {
+			this.initStatus();
 			this.initRanking();
 			this.initMessages();
 		},
-		
+
+		initStatus : function() {
+			if (!(this.attr('status'))) {
+				this.attr('status', 'waiting');
+			}
+		},
+
 		initRanking : function() {
 			var players = this.attr('players');
 			var ranking = [];
 			$.each(players, function(index, player) {
 				ranking.push({
-					id: player.attr('id'),
-					name: player.attr('name'),
-					position: index + 1,
-					score: 0
+					id : player.attr('id'),
+					name : player.attr('name'),
+					position : index + 1,
+					score : 0
 				});
 			});
 			this.attr('ranking', ranking);
 		},
-		
+
 		initMessages : function() {
-			this.attr('messages', [{
+			this.attr('messages', [ {
 				from : '小明',
 				content : '我们是害虫'
-			},{
+			}, {
 				from : '张三',
 				content : '快点啊，我等你等到花儿也谢了。'
-			},{
+			}, {
 				from : '李四',
 				content : '我是七星瓢虫'
-			},{
+			}, {
 				from : '小明',
 				content : '你们的水平太差了，我后悔跟你们一起玩了。'
-			},{
+			}, {
 				from : '小明',
 				content : '我们是害虫'
-			},{
+			}, {
 				from : '小明',
 				content : '我们是害虫'
-			},{
+			}, {
 				from : '小明',
 				content : '我们是害虫'
-			},{
+			}, {
 				from : '小明',
 				content : '我们是害虫'
-			},{
+			}, {
 				from : '小明',
 				content : '我们是害虫'
-			},{
+			}, {
 				from : '小明',
 				content : '我们是害虫'
-			},{
+			}, {
 				from : '小明',
 				content : '我们是害虫'
-			},{
+			}, {
 				from : '小明',
 				content : '我们是害虫'
-			},{
+			}, {
 				from : '小明',
 				content : '我们是害虫'
-			}]);
+			} ]);
+		},
+		
+		start : function() {
+			this.attr('status', 'ongoing');
 		}
 	});
 })();
--- public/js/components/game_panel.js
+++ public/js/components/game_panel.js
@@ -2,6 +2,11 @@
 	can.Control('Components.GamePanel', {}, {
 		init : function(element, options) {
 			element.html(can.view('mst/game_panel.mst', options.model));
+			if (options.model.attr('status') === 'waiting') {
+				this.showWaiting();
+			} else if (options.model.attr('status') === 'ongoing') {
+				this.showOngoing();
+			}
 		},
 
 		'.game-state-hide-button click' : function() {
@@ -12,6 +17,46 @@
 			} else {
 				gameStateArea.addClass('out').css('right', (-gameStateArea[0].offsetWidth) + 'px');
 				gameStateArea.find('.game-state-hide-button').html('<<');
+			}
+		},
+
+		'{model} status' : function(model, e, newStatus, oldStatus) {
+			if (newStatus === 'waiting') {
+				this.showWaiting();
+			} else if (newStatus === 'ongoing') {
+				this.showOngoing(oldStatus === 'waiting');
+			}
+		},
+		
+		'.game-start-button click' : function() {
+			this.options.model.start();
+		},
+
+		showWaiting : function() {
+			$('.game-main-area').empty().html(can.view('mst/game_waiting.mst', this.options.model));
+		},
+
+		showOngoing : function(countdown) {
+			var mainArea = $('.game-main-area');
+			var showChessbord = function() {};
+			if(countdown) {
+				mainArea.empty().html('<div class="game-countdown-number" style="display:none;"></div>');
+				var leftSeconds = 5;
+				var countdownElement = mainArea.find('.game-countdown-number');
+				countdownElement.html(leftSeconds).fadeIn(500);
+				var nextNumber = function() {
+					countdownElement.hide();
+					countdownElement.html(--leftSeconds)
+					countdownElement.fadeIn(500);
+					if(leftSeconds > 0) {
+						setTimeout(nextNumber, 1000);
+					} else {
+						setTimeout(function() {
+							countdownElement.remove();
+						}, 1000)
+					}
+				};
+				setTimeout(nextNumber, 1000);
 			}
 		}
 	});
