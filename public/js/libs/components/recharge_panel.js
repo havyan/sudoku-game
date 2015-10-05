0422243fe73079cf5a29bb35518a7a12e527d83a
havyan <havyan@gmail.com>
2015-10-05 21:37:20 +0800

    Pending

--- public/js/libs/components/recharge_panel.js
+++ public/js/libs/components/recharge_panel.js
@@ -2,6 +2,15 @@
   can.Control('Components.RechargePanel', {}, {
     init : function(element, options) {
       element.html(can.view('/js/libs/mst/recharge_panel.mst', {}));
-    }
+      this.paginationModel = new Models.PaginationModel();
+      this.paginationBar = new Components.PaginationBar(element.find('.records-pagination-bar'), {
+        model : this.paginationModel
+      });
+    },
+
+    '.navigator .item click' : function(e) {
+      e.addClass('active').siblings('.item').removeClass('active');
+      this.element.find('.recharge-container .item').removeClass('active').filter('.' + e.data('target')).addClass('active');
+    },
   });
 })();
