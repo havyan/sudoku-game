f019f6bd6b891f9afb301c0184bc2fad3a1f4fb9
havyan <havyan@gmail.com>
2017-03-09 12:07:36 +0800

    Fix  bugs

--- src/main/routers/page.js
+++ src/main/routers/page.js
@@ -138,6 +138,13 @@ module.exports = function(router) {
   });
 
   router.get('/logout', function(req, res) {
+    var account = req.session.account;
+    winston.info('Try to quit ongoing game before logout');
+    global.gameManager.playerQuit(account, function(error) {
+      if (error) {
+        winston.error('Error when quiting game before logout with account: ' + account);
+      }
+    });
     req.session.account = undefined;
     res.clearCookie('account');
     res.clearCookie('password');
