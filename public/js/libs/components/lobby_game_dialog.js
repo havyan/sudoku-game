1860f4840f4745183eb485c83a09542b0fbb4571
havyan <havyan@gmail.com>
2017-06-19 14:20:46 +0800

    Pending

--- public/js/libs/components/lobby_game_dialog.js
+++ public/js/libs/components/lobby_game_dialog.js
@@ -24,11 +24,11 @@
 
     initDefault : function() {
       var levels = this.model.attr('levels');
-      $('.value.level select').val(levels.attr((levels.length - 1) + '.code'));
-      $('.value.step-time select').val(this.model.attr('stepTimes.0.total'));
-      $('.value.duration select').val(this.model.attr('durations.0'));
-      $('.value.capacity select').val(this.model.attr('capacities.0'));
-      $('.value.wait-time select').val(this.model.attr('waitTimes.0'));
+      $('.lobby-game-modal .value.level select').val(levels.attr((levels.length - 1) + '.code'));
+      $('.lobby-game-modal .value.step-time select').val(this.model.attr('stepTimes.0.total'));
+      $('.lobby-game-modal .value.duration select').val(this.model.attr('durations.0'));
+      $('.lobby-game-modal .value.capacity select').val(this.model.attr('capacities.0'));
+      $('.lobby-game-modal .value.wait-time select').val(this.model.attr('waitTimes.0'));
     },
 
     createModel : function() {
@@ -45,7 +45,7 @@
       this.element.find('.lobby-game-modal').modal('hide');
     },
 
-    '.lobby-modal .close click' : function(element) {
+    '.lobby-game-modal .close click' : function(element) {
       this.close();
     },
 
@@ -56,25 +56,25 @@
 
     getParams : function() {
       return {
-        level : this.element.find('.value.level select').val(),
-        stepTime : parseInt(this.element.find('.value.step-time select').val()),
-        duration : parseFloat(this.element.find('.value.duration select').val()),
-        capacity : parseInt(this.element.find('.value.capacity select').val()),
-        startMode : this.element.find('.value.start-mode input:checked').data('value'),
-        waitTime : parseInt(this.element.find('.value.wait-time select').val())
+        level : this.element.find('.lobby-game-modal .value.level select').val(),
+        stepTime : parseInt(this.element.find('.lobby-game-modal .value.step-time select').val()),
+        duration : parseFloat(this.element.find('.lobby-game-modal .value.duration select').val()),
+        capacity : parseInt(this.element.find('.lobby-game-modal .value.capacity select').val()),
+        startMode : this.element.find('.lobby-game-modal .value.start-mode input:checked').data('value'),
+        waitTime : parseInt(this.element.find('.lobby-game-modal .value.wait-time select').val())
       };
     },
 
-    '.lobby-modal .confirm click' : function(element) {
+    '.lobby-game-modal .confirm click' : function(element) {
       if (this.callback) {
         this.callback(this.getParams());
       }
       this.close();
     },
 
-    '.value.capacity select change' : function(e) {
-      var $auto = this.element.find('.value.start-mode input[data-value=auto]');
-      var $manual = this.element.find('.value.start-mode input[data-value=manual]');
+    '.lobby-game-modal .value.capacity select change' : function(e) {
+      var $auto = this.element.find('.lobby-game-modal .value.start-mode input[data-value=auto]');
+      var $manual = this.element.find('.lobby-game-modal .value.start-mode input[data-value=manual]');
       if (e.val() == 99) {
         $manual.prop("checked", true);
         $auto.attr('disabled', true);
