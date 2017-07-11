d84b693d014eb10b6583f3c667fc5e06a01c408e
havyan <havyan@gmail.com>
2017-07-11 10:48:03 +0800

    Pending

---
+++ public/js/libs/stats.js
@@ -0,0 +1,9 @@
+(function() {
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?83bb7199f4ea6a0c713641c384384bc8";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+})();
