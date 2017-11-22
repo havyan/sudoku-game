136feb1d8fdf14b115cd37223693b1a17c94a89e
Haowei Yan <haowei.yan@spirent.com>
2017-11-21 21:31:37 -0600

    Pending

--- public/js/libs/treasure/main.js
+++ public/js/libs/treasure/main.js
@@ -61,7 +61,7 @@
         userClass: 'cool',
         autoClose: false,
         actions: [{
-          name: '开始游戏',
+          name: it('page:lobby.start_game'),
           callback: function() {
             Rest.Game.createTreasureGame();
             window.open('/treasure/table/' + data.user.account, '_blank');
